9e1fe4a9d8215d1b2541e1dfe40be343bafa199f
MatthewPass24 <matthewpass24@gmail.com>
2024-10-15 21:11:17 -0400

    started timeline for the schedule page and can now hover to get trunk details

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-    $('.infoBtn').on('click', function() {
-        let description = $(this).data('description');
-        $('#modalDescription').text(description);
-        $('#infoModal').modal('show');
-    });
+// Show description on hover
+$('.gallery').on('mouseenter', function() {
+    $(this).find('.description').fadeIn();
+}).on('mouseleave', function() {
+    $(this).find('.description').fadeOut();
+});
+//timeline functionality on schedule page
+$('.button a').on('click', function() {
+            const info = $(this).data('info'); // Get the data-info attribute
+            $('#modalDescription').text(info); // Set the modal content
+            $('#infoModal').modal('show'); // Show the modal
+        });
 })
-//
